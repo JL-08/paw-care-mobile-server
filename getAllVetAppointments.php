c673ac8046eaf8d20f0ea59cfddc7e12e907c61b
JL-08 <janellelacsamana@gmail.com>
2021-07-03 23:56:38 +0800

    add end date

--- getAllVetAppointments.php
+++ getAllVetAppointments.php
@@ -6,7 +6,7 @@ $vet_id = $json["vet_id"];
 
 $data = array(); 
  
-$sql = "SELECT a.id, a.start_date, a.type, a.reason, a.status, a.meeting_link, u.name, u.mobile_num, p.name, p.age, p.type, p.breed, p.weight, p.height
+$sql = "SELECT a.id, a.start_date, a.end_date, a.type, a.reason, a.status, a.meeting_link, u.name, u.mobile_num, p.name, p.age, p.type, p.breed, p.weight, p.height
         FROM appointments AS a
         JOIN users as u ON a.user_id=u.id
         JOIN pets as p ON a.pet_id=p.id
@@ -14,13 +14,14 @@ $sql = "SELECT a.id, a.start_date, a.type, a.reason, a.status, a.meeting_link, u
 
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-$stmt->bind_result($id, $start_date, $type, $reason, $status, $meeting_link, $user_name, $user_mobile_num, $pet_name, $pet_age, $pet_type, $pet_breed, $pet_weight, $pet_height);
+$stmt->bind_result($id, $start_date, $end_date, $type, $reason, $status, $meeting_link, $user_name, $user_mobile_num, $pet_name, $pet_age, $pet_type, $pet_breed, $pet_weight, $pet_height);
  
 while($stmt->fetch()){
  
  $temp = [
  'id'=>$id,
  'start_date'=>$start_date,
+ 'end_date'=>$end_date,
  'type'=>$type,
  'reason'=>$reason,
  'status'=>$status,
